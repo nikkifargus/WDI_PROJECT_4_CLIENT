adee8f35a25b7b9493a01a28b34709d67ddaf2b5
nikkifargus <nikki@fargus.co.uk>
2017-09-05 17:37:41 +0100

    almost at mvp just need to add map

--- src/js/controllers/lessons/show.controller.js
+++ src/js/controllers/lessons/show.controller.js
@@ -1,10 +1,42 @@
 angular
-  .module('project4')
-  .controller('LessonsShowCtrl', LessonsShowCtrl);
+.module('project4')
+.controller('LessonsShowCtrl', LessonsShowCtrl);
 
-LessonsShowCtrl.$inject = ['Lesson', '$stateParams'];
-function LessonsShowCtrl(Lesson, $stateParams) {
+LessonsShowCtrl.$inject = ['Lesson', '$stateParams', 'CurrentUserService'];
+function LessonsShowCtrl(Lesson, $stateParams, CurrentUserService) {
   const vm = this;
 
+  vm.attending = userAttending;
+  vm.notAttending = notAttending;
   vm.lesson = Lesson.get({ id: $stateParams.id });
+
+  
+
+  function userAttending(){
+    // if current user id is already in the array of user_ids - nothing
+    // if it ISNT in the user_ids array - push id in and update
+
+    console.log(CurrentUserService.currentUser.id);
+    vm.lesson.user_ids.push(CurrentUserService.currentUser.id);
+    vm.lesson.users.push(CurrentUserService.currentUser.id);
+    // console.log(vm.lesson.users);
+    Lesson
+    .update({id: $stateParams.id}, {lesson: vm.lesson})
+    .$promise
+    .then((data2) => {
+      console.log('2',data2);
+      console.log(vm.lesson.users);
+    });
+  }
+
+  function notAttending() {
+    vm.lesson.user_ids.splice(vm.lesson.user_ids.indexOf(CurrentUserService.currentUser.id), 1);
+    vm.lesson.users.splice(vm.lesson.users.indexOf(CurrentUserService.currentUser.id), 1);
+    Lesson
+    .update({id: $stateParams.id}, {lesson: vm.lesson})
+    .$promise
+    .then(() => {
+
+    });
+  }
 }
--- src/js/factories/lesson.factory.js
+++ src/js/factories/lesson.factory.js
@@ -1,10 +1,12 @@
 angular
-  .module('project4')
-  .factory('Lesson', Lesson);
+.module('project4')
+.factory('Lesson', Lesson);
 
 Lesson.$inject = ['API', '$resource'];
 function Lesson(API, $resource){
   return $resource(`${API}/lessons/:id`, { id: '@_id'}, {
-    'update': { method: 'PUT' }
+    'update': { method: 'PUT' },
+    'attending': { method: 'PUT'}
+    // 'removeattending': { method: 'GET', url: `${API}/lessons/:id/removeattending` }
   });
 }
