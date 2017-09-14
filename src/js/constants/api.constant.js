734c5559f9174b319c95ffc2cf8351bad2115026
nikkifargus <nikki@fargus.co.uk>
2017-09-14 16:00:03 +0100

    removed yarn.lock

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,3 +1,4 @@
 angular
   .module('project4')
-  .constant('API', 'http://localhost:3000/api');
+  // .constant('API', 'http://localhost:3000/api');
+  .constant('API', 'https://fitness-fun.herokuapp.com/api');
